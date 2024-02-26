25a877500a47729408cc0f4c29e9b93190a96c57
Quentin Vansteene <vansteene.quentin@gmail.com>
2024-02-26 10:59:36 +0100

    general styling, and home specialties

--- altradRemoveInsul.js
+++ altradRemoveInsul.js
@@ -1,3 +1,5 @@
+document.documentElement.style.setProperty('--scrollbar-width', (window.innerWidth - document.documentElement.clientWidth) + "px");
+
 $(function(){
 
 });
--- config.js
+++ config.js
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+	'primary': 'color(green)',
+	'btn':{
+		'radius': '50px',
+		'border-size': '1px',
+		'uppercase': false,
+	},
+	'input':{
+		'radius': '2em',
+		'border-size': '1px',
+	}
+};
