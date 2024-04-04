0966cc7fb346dfc4b68d91cb04f7a1a0ae45c069
Quentin Vansteene <vansteene.quentin@gmail.com>
2024-04-04 18:36:54 +0200

    set config

--- config.js
+++ config.js
@@ -1,5 +1,21 @@
 module.exports = {
+	'colors': {
+		'green': '#438D2F',
+	},
 	'primary': 'color(green)',
+
+	'body': {
+		'font-size'		: '18px',
+		'font-size-sm'	: '16px',
+		'font-size-xxs'	: '14px',
+		'line-height' 	: '1.45',
+	},
+	'header':{
+		'font-size'		: '16px',
+		// 'font-color'	: 'color(white)',
+	},
+	'radius': '2em',
+
 	'btn':{
 		'radius': '50px',
 		'border-size': '1px',
